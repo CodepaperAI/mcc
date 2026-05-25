fee265c4fca4c81cf2794a91788886c705fa75c2
jagat-developer <jagat@codepaper.com>
2026-05-25 10:19:39 -0400

    blog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import Script from "next/script";
 import "./globals.css";
 
+const GTM_ID = "GTM-MQ87X9QN";
 const GOOGLE_ADS_ID = "AW-18057040233";
 const GOOGLE_ADS_SHARED_ID = "AW-18125449559";
 
@@ -39,6 +40,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script id="gtm-script" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`}
+        </Script>
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ADS_ID}`}
           strategy="afterInteractive"
@@ -52,6 +60,14 @@ gtag('config', '${GOOGLE_ADS_SHARED_ID}');`}
         </Script>
       </head>
       <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
       </body>
     </html>
